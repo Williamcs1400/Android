804c16af47d263a83d8ee9d99929797859df59ca
Williamcs1400 <williamcs1400@gmail.com>
2020-08-04 20:44:49 -0300

    Começando Instagram-Clone

--- 15-Instagram/app/src/main/java/com/williamcoelho/instagram/activity/MainActivity.java
+++ 15-Instagram/app/src/main/java/com/williamcoelho/instagram/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.williamcoelho.instagram;
+package com.williamcoelho.instagram.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.williamcoelho.instagram.R;
 
 public class MainActivity extends AppCompatActivity {
 
