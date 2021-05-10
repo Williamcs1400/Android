2f1578fc36f9fc86ba6bbde274218818e3260fd7
Williamcs1400 <williamcs1400@gmail.com>
2021-05-10 19:43:34 -0300

    Iniciando

---
+++ X-RequisicoesHTTP/app/src/main/java/com/william/requisicoeshttp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.william.requisicoeshttp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textResultado;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
