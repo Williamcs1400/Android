fedfba1e021028ccc2d1d93e484d86620ee86941
Williamcs1400 <williamcs1400@gmail.com>
2021-05-10 21:20:22 -0300

    Requisição terminada

--- X-RequisicoesHTTP/app/src/main/java/com/william/requisicoeshttp/MainActivity.java
+++ X-RequisicoesHTTP/app/src/main/java/com/william/requisicoeshttp/MainActivity.java
@@ -2,21 +2,86 @@ package com.william.requisicoeshttp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textResultado;
-    private Button button;
+    private Button buttonPesquisar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textResultado = findViewById(R.id.textView);
+        buttonPesquisar = findViewById(R.id.button);
 
-
+        buttonPesquisar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyTask task = new MyTask();
+                String urlApi = "https://blockchain.info/ticker";
+                task.execute(urlApi);
+            }
+        });
     }
+
+    class MyTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String stringUrl =  strings[0];
+            InputStream inputStream = null;
+            InputStreamReader inputStreamReader = null;
+            StringBuffer stringBuffer = new StringBuffer();
+
+            try {
+
+                URL url = new URL(stringUrl);
+                HttpURLConnection conect = (HttpURLConnection) url.openConnection();
+
+                inputStream = conect.getInputStream();
+                inputStreamReader =  new InputStreamReader(inputStream);
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                Log.i("teste", "reader: " + reader);
+                String line = "";
+                while((line = reader.readLine()) != null){
+                    stringBuffer.append(line);
+                    Log.i("teste", line);
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                Log.i("teste", "MalformedURLException");
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.i("teste", "IOException");
+            }
+
+            return stringBuffer.toString();
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            textResultado.setText(s);
+        }
+    }
+
 }
